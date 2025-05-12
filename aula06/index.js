0669e3b766ca05bea91cc78df179c02082b5211f
Seu Nome <seu.email@example.com>
2025-05-12 10:24:07 -0300

    Aula06

---
+++ aula06/index.js
@@ -0,0 +1,7 @@
+let nome = "Peixe";
+
+console.log("Eu gosto de comer", nome);
+
+//Não podemos criar variáveis com palavras reservadas, espaços, traços e nem começar com números
+//Devem ter nomes significativos para facilitar no entendimento na hora de escrever e dar manutenção no código.
+//Utilize let

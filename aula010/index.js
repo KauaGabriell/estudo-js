42fe583fd2f656eefbff9fe41428d6ecf4d692b3
Seu Nome <seuemail@example.com>
2025-05-13 15:40:49 -0300

    Aula010(Tipos de Dados)

---
+++ aula010/index.js
@@ -0,0 +1,9 @@
+const nome1 = 'Peixe';  //String
+const nome2 = "Peixe";  //String
+const nome3 = `Peixe`; //String
+const num = 10; //Number
+const num2 = 10.5 //Number
+let nomeAluno; //Undefined, não aponta para local nenhum da memória
+let sobrenomeAluno = null; //Não aponta para local nenhum da memória, eu escolho que isso aconteça.
+const bolean = true; //Verdadeiro (true) ou falso(false)
+

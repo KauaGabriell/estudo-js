09662527946d18a9e6781bf06c0b8d3af77c1270
Seu Nome <seuemail@example.com>
2025-05-13 17:41:42 -0300

    Aula14(Métodos de String básicos)

---
+++ aula014/index.js
@@ -0,0 +1,22 @@
+let Umastring = "Apenas um teste";
+console.log("Deixar tudo maiúsculo.")
+console.log(Umastring.toUpperCase());
+console.log('----');
+
+console.log("Deixar tudo minúsculo.")
+console.log(Umastring.toLowerCase());
+console.log('----');
+
+console.log("Replace(Substituir)")
+console.log(Umastring.replace('teste', 'show'));
+console.log('----');
+
+console.log("Slice(Cortar,fatiar)")
+console.log(Umastring.slice(0, 6));
+console.log(Umastring.slice(-5));
+console.log('----');
+
+console.log("Saber quantidade de caracteres");
+console.log(Umastring.length);
+
+

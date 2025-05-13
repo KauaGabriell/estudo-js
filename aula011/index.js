a132ae1ef0464938a97cc10eb1b8efdbdaafdb17
Seu Nome <seuemail@example.com>
2025-05-13 16:25:40 -0300

    Aula011(Operadores)

---
+++ aula011/index.js
@@ -0,0 +1,20 @@
+//Operadores aritméticos
+
+//+ -> Adição e concatenação;
+const a = 2;
+const b = 3;
+const c = b + a;
+
+//- Subtração
+const d = b - a;
+
+//* Multiplicação e ** Potencialização;
+const multiplicação = a * b;
+const potencia = a**b;
+
+// / - Divisão
+const divisao = a / b;
+
+//% - resto da divisão
+const resto = a % a;
+

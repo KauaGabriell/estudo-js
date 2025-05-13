af8729175b71b7d79ed129d821d1ef6def5ef440
Seu Nome <seuemail@example.com>
2025-05-13 16:55:17 -0300

    Aula012(Funções do Navegador + Exercício)

---
+++ aula012/index.js
@@ -0,0 +1,8 @@
+let num1 = prompt("Digite o 1º Número:  ");
+let num2 = prompt("Digite o 2º Número: ");
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+const resultado = num1 + num2;
+alert(`O resultado da soma é ${resultado}`);

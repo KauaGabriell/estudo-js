fc8b62177613a183a4995fc3ffb883713887bc99
Seu Nome <seu.email@example.com>
2025-05-16 11:55:52 -0300

    Aula016(Math)

---
+++ aula16/index.js
@@ -0,0 +1,12 @@
+let num1 = 9.895;
+//let num2 = Math.floor(num1); Arredonda para cima
+//let num2 = Math.ceil(num1); Arredonda para baixo
+//let num2 = Math.round(num1); Arredonda de forma automática.
+//Math.max = Pega o maior valor dos valores passados.
+//Math.min = Pega o menor valor dos valores passados.
+//Math.random = Gera números aleatórios.
+
+const aleatorio = Math.round(Math.random() * ((100 - 1) + 1)); // Fazemos essa estrutura para gerar números aleatórios entre um intervalo.(No caso aqui, entre 1 e 100);
+console.log(aleatorio);
+
+

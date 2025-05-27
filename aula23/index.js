aa00f511b74d7fc39c7bcb85f7edb83f1d2b27e3
Seu Nome <seu.email@example.com>
2025-05-27 11:30:14 -0300

    Aula21(Operadores de Comparação)

---
+++ aula23/index.js
@@ -0,0 +1,13 @@
+/**
+ * | Operador | Nome                                  | Exemplo     | Resultado           |
+| -------- | ------------------------------------- | ----------- | ------------------- |
+| `==`     | Igual (valor)                         | `5 == '5'`  | `true` *(coerção)*  |
+| `===`    | Estritamente igual (valor e tipo)     | `5 === '5'` | `false`             |
+| `!=`     | Diferente (valor)                     | `5 != '5'`  | `false` *(coerção)* |
+| `!==`    | Estritamente diferente (valor e tipo) | `5 !== '5'` | `true`              |
+| `>`      | Maior que                             | `10 > 3`    | `true`              |
+| `<`      | Menor que                             | `2 < 5`     | `true`              |
+| `>=`     | Maior ou igual a                      | `5 >= 5`    | `true`              |
+| `<=`     | Menor ou igual a                      | `4 <= 3`    | `false`             |
+
+ */

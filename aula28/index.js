2a948d2dbce273da2ffe89bfbff386ef1030b960
Seu Nome <seuemail@example.com>
2025-06-02 18:22:10 -0300

    Aula28(Operação Ternária)

---
+++ aula28/index.js
@@ -0,0 +1,6 @@
+//Condição ? 'Valor para verdadeiro' : 'Valor para falso'
+
+const pontos = 9000;
+const nivelPontos = pontos >= 1000 ? 'Usuário VIP' : 'Usuário Normal';
+
+console.log(nivelPontos);

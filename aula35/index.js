a46b1eafb1f9c50a61fd446dd8d00b69d2f4eebb
Seu Nome <seu.email@example.com>
2025-06-11 12:07:00 -0300

    Aula33(for)

---
+++ aula35/index.js
@@ -0,0 +1,11 @@
+/**Exemplo Padrão de for */
+for(let i = 0; i < 10; i ++){
+    console.log(i);
+}
+/**For percorrendo um array e exibindo seus elementos */
+const frutas = ['maçã', 'banana', 'laranja', 'morango', 'abacaxi'];
+
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i]);
+}
+

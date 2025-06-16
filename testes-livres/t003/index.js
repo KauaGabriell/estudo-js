15b4aa5857ba95b92a8d2309a0eecfcf917c5720
Seu Nome <seu.email@example.com>
2025-06-16 13:13:15 -0300

    Começando T003 (Alguns métodos de Array Avançado)

---
+++ testes-livres/t003/index.js
@@ -0,0 +1,23 @@
+const produtos = [
+    {
+        nome: "Notebook",
+        preco: 3500.00
+    },
+    {
+        nome: "Smartphone",
+        preco: 2000.00
+    },
+    {
+        nome: "Tablet",
+        preco: 1500.00
+    },
+    {
+        nome: "Monitor",
+        preco: 800.00
+    },
+    {
+        nome: "Teclado",
+        preco: 150.00
+    }
+];
+

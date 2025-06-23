5e78dbe592c3fa3e29371bdb06b8aa799d78b311
Kaua Gabriell Nazario de Azevedo <kaua.azevedo@unievangelica.edu.br>
2025-06-23 19:58:33 -0300

    Aula 38(for of)

---
+++ aula38/index.js
@@ -0,0 +1,9 @@
+const nomes = ['peixe', 'mari', 'maya'];
+
+for(let i of nomes){
+    console.log(i);
+}
+
+//Diferença entre for in e for of
+/* No for in nós pegamos o índice também ou seja, no exemplo acima teriámos que exibir da seguinte forma "console.log(nomes[i])"*/
+/* No for of nós pegamos direto o valor.*/

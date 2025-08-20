24cc58de271b04bbb641622304b36ab41da68a65
Kauã Gabriell <kauagabriell64@gmail.com>
2025-08-20 13:47:30 -0300

    Aula70(Revisão de Objetos)

---
+++ aula70/main.js
@@ -0,0 +1,16 @@
+/**Revisão de Objetos */
+
+/*Criando Objeto*/ 
+
+//Literal (Forma literal de criar um objeto)
+const pessoa = {
+    nome: 'Peixe',
+    idade: 21,
+};
+
+//Construtor
+function Pessoa(nome, sobrenome){
+    this.nome = nome
+    this.sobrenome = sobrenome
+};
+

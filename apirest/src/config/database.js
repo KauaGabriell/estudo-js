4917c7c09dcaf9e6b7045c303b2e9476010b0387
Kauã Gabriell <kauagabriell64@gmail.com>
2026-01-20 16:13:44 -0300

    :sparkles: feat: Atualiza a configuração do banco de dados para usar MySQL e modifica a porta do servidor para ser uma variável de ambiente

--- apirest/src/config/database.js
+++ apirest/src/config/database.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  dialect: "mariadb", // ou 'mysql'
+  dialect: "mysql", // ou 'mysql'
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,

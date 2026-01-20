cd756d7b5642fc7f8eb36dc2bb567c370ffdc601
Kauã Gabriell <kauagabriell64@gmail.com>
2026-01-20 11:31:41 -0300

    :sparkles: feat: Implementando upload de fotos com multer no controlador.

--- apirest/src/routes/foto.routes.js
+++ apirest/src/routes/foto.routes.js
@@ -1,13 +1,9 @@
 import { Router } from "express";
-import multer from "multer";
 
-import multerConfig from "../config/multer.js";
 import FotoController from "../controllers/Foto.controller.js";
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post("/", upload.single('foto'), FotoController.index);
+router.post("/", FotoController.index);
 
 export default router;
